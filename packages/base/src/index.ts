8e5a0355bf941293a6bb0b0e7a2b3fac73347a83
SukkaW <isukkaw@gmail.com>
2023-09-23 00:31:03 +0800

    feat(legacy/base): eslint flat

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -1,21 +1,29 @@
 import { best_practices, errors, es6, style, variables, sukka } from '@eslint-sukka/shared';
+import { FlatESLintConfigItem } from 'eslint-define-config';
 
-export default {
-  extends: ['eslint:recommended'],
-  plugins: Array.from(new Set([
-    ...best_practices.plugins,
-    ...errors.plugins,
-    ...es6.plugins,
-    ...style.plugins,
-    ...variables.plugins,
-    ...sukka.plugins
-  ])),
-  rules: {
-    ...best_practices.rules,
-    ...errors.rules,
-    ...es6.rules,
-    ...style.rules,
-    ...variables.rules,
-    ...sukka.rules
-  }
+// @ts-expect-error -- no types
+import eslintJs from '@eslint/js';
+
+export const base = (): FlatESLintConfigItem[] => {
+  return [{
+    plugins: {
+      ...best_practices.plugins,
+      ...errors.plugins,
+      ...es6.plugins,
+      ...style.plugins,
+      ...variables.plugins,
+      ...sukka.plugins
+    },
+    rules: {
+      ...best_practices.rules,
+      ...errors.rules,
+      ...es6.rules,
+      ...style.rules,
+      ...variables.rules,
+      ...sukka.rules,
+
+      // eslint:recommended
+      ...eslintJs.configs.recommended.rules,
+    }
+  }]
 };
--- packages/legacy/src/index.ts
+++ packages/legacy/src/index.ts
@@ -1,44 +1,50 @@
 import { best_practices, errors, es6, style, variables, sukka } from '@eslint-sukka/shared';
+import { FlatESLintConfigItem } from 'eslint-define-config';
 
-export default {
-  extends: ['eslint:recommended'],
-  plugins: Array.from(new Set([
-    ...best_practices.plugins,
-    ...errors.plugins,
-    ...es6.plugins,
-    ...style.plugins,
-    ...variables.plugins,
-    ...sukka.plugins
-  ])),
-  rules: {
-    ...best_practices.rules,
-    ...errors.rules,
-    ...es6.rules,
-    ...style.rules,
-    ...variables.rules,
-    ...sukka.rules,
+// @ts-expect-error -- no types
+import eslintJs from '@eslint/js';
 
-    'prefer-numeric-literals': 'off',
-    'no-restricted-properties': ['error', {
-      object: 'arguments',
-      property: 'callee',
-      message: 'arguments.callee is deprecated'
-    }, {
-      property: '__defineGetter__',
-      message: 'Please use Object.defineProperty instead.'
-    }, {
-      property: '__defineSetter__',
-      message: 'Please use Object.defineProperty instead.'
-    }],
-    'no-var': 'off',
-    'prefer-object-spread': 'off',
-    strict: ['error', 'safe']
-  },
-  env: {
-    browser: true,
-    node: true,
-    amd: false,
-    mocha: false,
-    jasmine: false
-  }
+import globals from 'globals';
+
+export const legacy = (): FlatESLintConfigItem[] => {
+  return [{
+    plugins: {
+      ...best_practices.plugins,
+      ...errors.plugins,
+      ...es6.plugins,
+      ...style.plugins,
+      ...variables.plugins,
+      ...sukka.plugins
+    },
+    rules: {
+      ...best_practices.rules,
+      ...errors.rules,
+      ...es6.rules,
+      ...style.rules,
+      ...variables.rules,
+      ...sukka.rules,
+
+      // eslint:recommended
+      ...eslintJs.configs.recommended.rules,
+
+      'prefer-numeric-literals': 'off',
+      'no-restricted-properties': ['error', {
+        object: 'arguments',
+        property: 'callee',
+        message: 'arguments.callee is deprecated'
+      }, {
+          property: '__defineGetter__',
+          message: 'Please use Object.defineProperty instead.'
+        }, {
+          property: '__defineSetter__',
+          message: 'Please use Object.defineProperty instead.'
+        }],
+      'no-var': 'off',
+      'prefer-object-spread': 'off',
+      strict: ['error', 'safe']
+    },
+    languageOptions: {
+      globals: globals.browser
+    }
+  }]
 };
