10cb822c2fec736988ee8cf122db1de83476c6ba
SukkaW <isukkaw@gmail.com>
2023-09-25 14:47:15 +0800

    chore: make eslint happy

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,30 @@
 
 const { sukka, constants } = require('eslint-config-sukka');
 
-module.exports = sukka({
-  ignores: {
-    customGlobs: [
-      ...constants.GLOB_EXCLUDE,
-      '**/generated*'
-    ]
+module.exports = sukka(
+  {
+    ignores: {
+      customGlobs: [
+        ...constants.GLOB_EXCLUDE,
+        '**/generated*'
+      ]
+    },
+    node: {
+      enable: true
+    },
+    ts: {
+      enable: true,
+      tsconfigPath: './tsconfig.json'
+    }
   },
-  node: {
-    enable: true
-  },
-  ts: {
-    enable: true,
-    tsconfigPath: './tsconfig.json'
+  {
+    plugins: {
+      '@stylistic/migrate': require('@stylistic/eslint-plugin-migrate')
+    },
+    rules: {
+      '@stylistic/migrate/rules': 'error',
+      camelcase: 'off',
+      '@typescript-eslint/naming-convention': 'off'
+    }
   }
-}, {
-  plugins: {
-    '@stylistic/migrate': require('@stylistic/eslint-plugin-migrate')
-  },
-  rules: {
-    '@stylistic/migrate/rules': 'error',
-    camelcase: 'off',
-    '@typescript-eslint/naming-convention': 'off'
-  }
-});
+);
