e515b45c55b9b48d2151fd645acdf5aedd5bee7a
SukkaW <isukkaw@gmail.com>
2023-11-13 17:11:10 +0800

    chore(react): bundle `eslint-plugin-react` & `eslint-plugin-jsx-a11y`

--- packages/react/rollup.config.ts
+++ packages/react/rollup.config.ts
@@ -1,3 +1,22 @@
 import { createRollupConfig } from '@eslint-sukka/rollup-config';
 
-export default createRollupConfig(new URL('./package.json', import.meta.url));
+export default createRollupConfig(new URL('./package.json', import.meta.url), [], {
+  nodeResolve: true,
+  commonjs: true,
+  json: true,
+  alias: {
+    entries: {
+      'es-iterator-helpers': '@nolyfill/es-iterator-helpers',
+      'array-includes': '@nolyfill/array-includes',
+      'array.prototype.tosorted': '@nolyfill/array.prototype.tosorted',
+      'array.prototype.flat': '@nolyfill/array.prototype.flat',
+      'array.prototype.flatmap': '@nolyfill/array.prototype.flatmap',
+      'string.prototype.matchall': '@nolyfill/string.prototype.matchall',
+      'object.values': '@nolyfill/object.values',
+      'object.assign': '@nolyfill/object.assign',
+      'object.entries': '@nolyfill/object.entries',
+      'object.fromentries': '@nolyfill/object.fromentries',
+      'object.hasown': '@nolyfill/object.hasown'
+    }
+  }
+});
