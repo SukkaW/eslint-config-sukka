f54d1f271084a86112c9b5abf7e51878deed3acf
SukkaW <isukkaw@gmail.com>
2023-12-11 15:13:59 +0800

    perf(react): only bundle minimum rules

--- packages/eslint-plugin-react-jsx-a11y/src/index.ts
+++ packages/eslint-plugin-react-jsx-a11y/src/index.ts
@@ -1,8 +1,49 @@
 import type { ESLint } from 'eslint';
-// @ts-expect-error -- missing types
-import * as $eslint_plugin_react from 'eslint-plugin-react';
-// @ts-expect-error -- missing types
-import * as $eslint_plugin_jsx_a11y from 'eslint-plugin-jsx-a11y';
 
-export const eslint_plugin_react: ESLint.Plugin = $eslint_plugin_react;
-export const eslint_plugin_jsx_a11y: ESLint.Plugin = $eslint_plugin_jsx_a11y;
+// @ts-expect-error -- missing types
+import no_unsed_state from 'eslint-plugin-react/lib/rules/no-unused-state';
+// @ts-expect-error -- missing types
+import no_access_state_in_setstate from 'eslint-plugin-react/lib/rules/no-access-state-in-setstate';
+// @ts-expect-error -- missing types
+import no_unsed_class_component_methods from 'eslint-plugin-react/lib/rules/no-unused-class-component-methods';
+// @ts-expect-error -- missing types
+import jsx_pascal_case from 'eslint-plugin-react/lib/rules/jsx-pascal-case';
+// @ts-expect-error -- missing types
+import sort_comp from 'eslint-plugin-react/lib/rules/sort-comp';
+// @ts-expect-error -- missing types
+import jsx_no_script_url from 'eslint-plugin-react/lib/rules/jsx-no-script-url';
+
+// @ts-expect-error -- missing types
+import alt_text from 'eslint-plugin-jsx-a11y/lib/rules/alt-text';
+// @ts-expect-error -- missing types
+import aria_props from 'eslint-plugin-jsx-a11y/lib/rules/aria-props';
+// @ts-expect-error -- missing types
+import aria_proptypes from 'eslint-plugin-jsx-a11y/lib/rules/aria-proptypes';
+// @ts-expect-error -- missing types
+import aria_unsupported_elements from 'eslint-plugin-jsx-a11y/lib/rules/aria-unsupported-elements';
+// @ts-expect-error -- missing types
+import role_has_required_aria_props from 'eslint-plugin-jsx-a11y/lib/rules/role-has-required-aria-props';
+// @ts-expect-error -- missing types
+import role_supports_aria_props from 'eslint-plugin-jsx-a11y/lib/rules/role-supports-aria-props';
+
+export const eslint_plugin_react_minimal: ESLint.Plugin = {
+  rules: {
+    'no-unused-state': no_unsed_state,
+    'no-access-state-in-setstate': no_access_state_in_setstate,
+    'no-unused-class-component-methods': no_unsed_class_component_methods,
+    'jsx-pascal-case': jsx_pascal_case,
+    'sort-comp': sort_comp,
+    'jsx-no-script-url': jsx_no_script_url
+  }
+};
+
+export const eslint_plugin_jsx_a11y_minimal: ESLint.Plugin = {
+  rules: {
+    'alt-text': alt_text,
+    'aria-props': aria_props,
+    'aria-proptypes': aria_proptypes,
+    'aria-unsupported-elements': aria_unsupported_elements,
+    'role-has-required-aria-props': role_has_required_aria_props,
+    'role-supports-aria-props': role_supports_aria_props
+  }
+};
