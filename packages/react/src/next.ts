a9cde816325e53cb421872a808bfdfa78a3c0441
SukkaW <isukkaw@gmail.com>
2024-06-28 21:23:03 +0800

    fix(react): update eslint-plugin-next to be eslint 9 compat

--- packages/react/src/next.ts
+++ packages/react/src/next.ts
@@ -1,14 +1,12 @@
 import { constants, type FlatESLintConfigItem } from '@eslint-sukka/shared';
 
-import { fixupPluginRules } from '@eslint/compat';
-
 // @ts-expect-error -- missing -types
 import eslint_plugin_next from '@next/eslint-plugin-next';
 
 export const next = (): FlatESLintConfigItem[] => {
   return [{
     plugins: {
-      '@next/next': fixupPluginRules(eslint_plugin_next)
+      '@next/next': eslint_plugin_next
     },
     files: [
       constants.GLOB_TS,
