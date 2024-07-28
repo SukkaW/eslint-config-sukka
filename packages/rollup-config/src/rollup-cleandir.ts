2754cbc5e1041bf6e0f5649e1b9bf03a671f0abb
SukkaW <isukkaw@gmail.com>
2024-07-28 21:54:55 +0800

    fix(rollup): race condition in rendering

--- packages/rollup-config/src/rollup-cleandir.ts
+++ packages/rollup-config/src/rollup-cleandir.ts
@@ -4,10 +4,16 @@ import fsp from 'fs/promises';
 export function cleandir(): RollupPlugin {
   return {
     name: 'cleandir',
-    async renderStart(outputOptions) {
-      if (outputOptions.dir) {
-        await fsp.rm(outputOptions.dir, { recursive: true, force: true });
-        await fsp.mkdir(outputOptions.dir, { recursive: true });
+    renderStart: {
+      order: 'pre',
+      async handler(outputOptions) {
+        if (outputOptions.dir) {
+          await fsp.rm(outputOptions.dir, { recursive: true, force: true });
+          await fsp.mkdir(outputOptions.dir, { recursive: true });
+        }
+        if (outputOptions.file) {
+          await fsp.rm(outputOptions.file, { force: true });
+        }
       }
     }
   };
