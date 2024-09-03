bd52e992b8be1ba7cdc74c697f17e46e77841cb8
SukkaW <isukkaw@gmail.com>
2024-09-03 12:34:14 +0800

    chore: improve bundling

--- packages/eslint-config-sukka/rollup.config.ts
+++ packages/eslint-config-sukka/rollup.config.ts
@@ -1,6 +1,6 @@
 import { createRollupConfig } from '@eslint-sukka/rollup-config';
 
-export default createRollupConfig(new URL('./package.json', import.meta.url), ['acorn'], {
+export default createRollupConfig(new URL('./package.json', import.meta.url), [], {
   foxquire: true,
   nodeResolve: true,
   commonjs: true,
