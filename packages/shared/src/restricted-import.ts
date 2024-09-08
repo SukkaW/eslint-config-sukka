788d0812a3559202dcf4dbbde3c7385228529754
SukkaW <isukkaw@gmail.com>
2024-09-09 03:43:26 +0800

    feat: disallow `useCallback` from `react` (prefer `foxact`)

--- packages/shared/src/restricted-import.ts
+++ packages/shared/src/restricted-import.ts
@@ -62,7 +62,8 @@ const restrictedImportWithImportNames = [
   { name: 'lodash-es', message: 'Use https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser instead', importNames: ['cloneDeep'] },
   { name: 'uuid', importNames: ['v4'], message: 'Use https://www.npmjs.com/package/@lukeed/uuid instead' },
   { name: 'assert', importNames: ['deepEqual'], message: 'Use https://www.npmjs.com/package/dequal instead' },
-  { name: 'react', importNames: ['useLayoutEffect'], message: 'Use https://foxact.skk.moe/use-isomorphic-layout-effect instead' }
+  { name: 'react', importNames: ['useLayoutEffect'], message: 'Use https://foxact.skk.moe/use-isomorphic-layout-effect instead' },
+  { name: 'react', importNames: ['useCallback'], message: 'Use https://foxact.skk.moe/use-typescript-happy-callback instead' }
 ];
 
 export const RESTRICTED_IMPORT_JS = [
