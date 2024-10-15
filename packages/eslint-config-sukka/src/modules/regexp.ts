8b92ee9f466d7667455b47e56a2700814cae7f74
SukkaW <isukkaw@gmail.com>
2024-10-16 01:00:10 +0800

    chore: housekeeping

--- packages/eslint-config-sukka/src/modules/regexp.ts
+++ packages/eslint-config-sukka/src/modules/regexp.ts
@@ -5,15 +5,17 @@ import type { FlatESLintConfigItem } from '@eslint-sukka/shared';
 import * as eslint_plugin_regexp from 'eslint-plugin-regexp';
 import { configs as eslint_pluin_regexp_configs } from 'eslint-plugin-regexp';
 
-export const regexp = (): FlatESLintConfigItem[] => [{
-  name: 'sukka/regexp',
-  plugins: {
-    sukka: memo(eslint_plugin_sukka, 'eslint-plugin-sukka'),
-    regexp: memo(eslint_plugin_regexp, 'eslint-plugin-regexp')
-  },
-  rules: {
-    'sukka/unicorn/better-regex': 'error', // RegEx[]
-    ...eslint_pluin_regexp_configs['flat/recommended'].rules,
-    'regexp/strict': 'off' // we accepts Annex B regex from better-regex
-  }
-}];
+export function regexp(): FlatESLintConfigItem[] {
+  return [{
+    name: 'sukka/regexp',
+    plugins: {
+      sukka: memo(eslint_plugin_sukka, 'eslint-plugin-sukka'),
+      regexp: memo(eslint_plugin_regexp, 'eslint-plugin-regexp')
+    },
+    rules: {
+      'sukka/unicorn/better-regex': 'warn', // RegEx[]
+      ...eslint_pluin_regexp_configs['flat/recommended'].rules,
+      'regexp/strict': 'off' // we accepts Annex B regex from better-regex
+    }
+  }];
+}
