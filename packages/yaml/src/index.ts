efc1f3c53df84f23d90eedafe795424ad9007be3
SukkaW <isukkaw@gmail.com>
2025-03-09 22:03:38 +0800

    feat(yaml): add key sort

--- packages/yaml/src/index.ts
+++ packages/yaml/src/index.ts
@@ -2,10 +2,43 @@ import type { FlatESLintConfigItem } from '@eslint-sukka/shared';
 import eslint_plugin_yml from 'eslint-plugin-yml';
 
 export function yaml(): FlatESLintConfigItem[] {
+  const myCfg: FlatESLintConfigItem = {
+    files: ['pnpm-workspace.yaml'],
+    name: 'sukka/yaml/pnpm-workspace',
+    rules: {
+      'yaml/sort-keys': [
+        'error',
+        {
+          order: [
+            'packages',
+            'overrides',
+            'patchedDependencies',
+            'hoistPattern',
+            'catalog',
+            'catalogs',
+
+            'allowedDeprecatedVersions',
+            'allowNonAppliedPatches',
+            'configDependencies',
+            'ignoredBuiltDependencies',
+            'ignoredOptionalDependencies',
+            'neverBuiltDependencies',
+            'onlyBuiltDependencies',
+            'onlyBuiltDependenciesFile',
+            'packageExtensions',
+            'peerDependencyRules',
+            'supportedArchitectures'
+          ],
+          pathPattern: '^$'
+        }
+      ]
+    }
+  };
+
   const cfg = eslint_plugin_yml.configs['flat/recommended'];
   if (Array.isArray(cfg)) {
-    return cfg;
+    return [...cfg, myCfg];
   }
 
-  return [cfg as any];
+  return [cfg as FlatESLintConfigItem, myCfg];
 }
