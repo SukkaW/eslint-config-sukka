2cdca471b57679ee5dbb5c7c35f2fdf7b6fdb70f
SukkaW <isukkaw@gmail.com>
2025-03-09 22:18:18 +0800

    fix(yaml): ignore rules about comment
    https://github.com/ota-meshi/eslint-plugin-yml/issues/277

--- packages/yaml/src/index.ts
+++ packages/yaml/src/index.ts
@@ -2,43 +2,55 @@ import type { FlatESLintConfigItem } from '@eslint-sukka/shared';
 import eslint_plugin_yml from 'eslint-plugin-yml';
 
 export function yaml(): FlatESLintConfigItem[] {
-  const myCfg: FlatESLintConfigItem = {
-    files: ['pnpm-workspace.yaml'],
-    name: 'sukka/yaml/pnpm-workspace',
-    rules: {
-      'yaml/sort-keys': [
-        'error',
-        {
-          order: [
-            'packages',
-            'overrides',
-            'patchedDependencies',
-            'hoistPattern',
-            'catalog',
-            'catalogs',
+  const myCfg: FlatESLintConfigItem[] = [
+    {
+      files: ['*.yaml', '**/*.yaml', '*.yml', '**/*.yml'],
+      rules: {
+        // FIXME: https://github.com/ota-meshi/eslint-plugin-yml/issues/277
+        '@stylistic/js/spaced-comment': 'off'
+      }
+    },
+    {
+      files: ['pnpm-workspace.yaml'],
+      name: 'sukka/yaml/pnpm-workspace',
+      rules: {
+        'yml/sort-keys': [
+          'error',
+          {
+            order: [
+              'packages',
+              'overrides',
+              'patchedDependencies',
+              'hoistPattern',
+              'catalog',
+              'catalogs',
 
-            'allowedDeprecatedVersions',
-            'allowNonAppliedPatches',
-            'configDependencies',
-            'ignoredBuiltDependencies',
-            'ignoredOptionalDependencies',
-            'neverBuiltDependencies',
-            'onlyBuiltDependencies',
-            'onlyBuiltDependenciesFile',
-            'packageExtensions',
-            'peerDependencyRules',
-            'supportedArchitectures'
-          ],
-          pathPattern: '^$'
-        }
-      ]
+              'allowedDeprecatedVersions',
+              'allowNonAppliedPatches',
+              'configDependencies',
+              'ignoredBuiltDependencies',
+              'ignoredOptionalDependencies',
+              'neverBuiltDependencies',
+              'onlyBuiltDependencies',
+              'onlyBuiltDependenciesFile',
+              'packageExtensions',
+              'peerDependencyRules',
+              'supportedArchitectures'
+            ],
+            pathPattern: '^$'
+          }
+        ]
+      }
+    },
+    {
+
     }
-  };
+  ];
 
   const cfg = eslint_plugin_yml.configs['flat/recommended'];
   if (Array.isArray(cfg)) {
-    return [...cfg, myCfg];
+    return [...cfg, ...myCfg];
   }
 
-  return [cfg as FlatESLintConfigItem, myCfg];
+  return [cfg as FlatESLintConfigItem, ...myCfg];
 }
