1f9e35c2186c5f6fdbf1bae9d6056bcce7cc70d6
SukkaW <isukkaw@gmail.com>
2025-03-14 16:42:35 +0800

    feat: improve module replacements

--- packages/shared/src/restricted-import.ts
+++ packages/shared/src/restricted-import.ts
@@ -1,52 +1,66 @@
+const npm = (pkgName: string) => `https://www.npmjs.com/package/${pkgName}`;
+
 /** 福妮严选 */
 export const BETTER_ALTERNATIVES = Object.entries({
   // deep equal
-  'deep-equal': 'https://www.npmjs.com/package/dequal',
-  'fast-deep-equal': 'https://www.npmjs.com/package/dequal',
+  'deep-equal': npm('dequal'),
+  'fast-deep-equal': npm('dequal'),
   // ansi color / cursor
-  chalk: 'simple color https://www.npmjs.com/package/picocolors, hex color https://github.com/webdiscus/ansis',
-  kleur: 'https://www.npmjs.com/package/picocolors',
-  'ansi-escapes': 'https://www.npmjs.com/package/sisteransi',
+  chalk: `simple color ${npm('picocolors')}, hex color https://github.com/webdiscus/ansis`,
+  kleur: npm('picocolors'),
+  'ansi-escapes': npm('sisteransi'),
   // mime
-  'mime-db': 'https://www.npmjs.com/package/mrmime',
-  'mime-types': 'https://www.npmjs.com/package/mrmime',
+  'mime-db': npm('mrmime'),
+  'mime-types': npm('mrmime'),
   // lru
-  'lru-cache': 'w/o TTL https://www.npmjs.com/package/flru, w/ TTL TTL: https://www.npmjs.com/package/tiny-lru',
-  'tmp-cache': 'w/o TTL https://www.npmjs.com/package/flru, w/ TTL TTL: https://www.npmjs.com/package/tiny-lru',
+  'lru-cache': `w/o TTL ${npm('flru')}, w/ TTL TTL: ${npm('tiny-lru')}`,
+  'tmp-cache': `w/o TTL ${npm('flru')}, w/ TTL TTL: ${npm('tiny-lru')}`,
   // fs operations
   premove: 'Node.js built-in fs.rmdir & fs.rm API',
+  rimraf: 'Node.js built-in fs.rmdir & fs.rm API',
   mkdirp: 'Node.js built-in fs.mkdir API',
   'make-dir': 'Node.js built-in fs.mkdir API',
   'mk-dirs': 'Node.js built-in fs.mkdir API',
   // network
-  axios: 'https://www.npmjs.com/package/ky',
-  'node-fetch': 'https://www.npmjs.com/package/undici (preferred) or https://www.npmjs.com/package/node-fetch-native',
-  got: 'https://www.npmjs.com/package/undici and https://www.npmjs.com/package/async-retry',
-  'get-port': 'https://www.npmjs.com/package/get-port-please',
+  axios: npm('ky'),
+  'node-fetch': `${npm('undici')} (preferred) or ${npm('node-fetch-native')}`,
+  got: `${npm('undici')} and ${npm('async-retry')}`,
+  'get-port': npm('get-port-please'),
   // react
   'react-query': 'https://swr.vercel.app',
   '@tanstack/react-query': 'https://swr.vercel.app',
   '@tanstack/react-query-devtools': 'https://swr.vercel.app',
   ahooks: 'https://foxact.skk.moe and https://swr.vercel.app',
   // deep clone
-  'clone-deep': 'https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser',
-  'deep-copy': 'https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser',
-  'fast-copy': 'https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser',
-  'lodash.clonedeep': 'https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser',
-  clone: 'https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser',
+  'clone-deep': `${npm('rfdc')} for Node.js / ${npm('klona')} for Browser`,
+  'deep-copy': `${npm('rfdc')} for Node.js / ${npm('klona')} for Browser`,
+  'fast-copy': `${npm('rfdc')} for Node.js / ${npm('klona')} for Browser`,
+  'lodash.clonedeep': `${npm('rfdc')} for Node.js / ${npm('klona')} for Browser`,
+  clone: `${npm('rfdc')} for Node.js / ${npm('klona')} for Browser`,
   // misc
-  ms: 'https://www.npmjs.com/package/@lukeed/ms',
-  classnames: 'https://www.npmjs.com/package/clsx',
-  classcat: 'https://www.npmjs.com/package/clsx',
-  'p-limit': 'https://www.npmjs.com/package/superlock / https://www.npmjs.com/package/@henrygd/queue / https://www.npmjs.com/package/async-sema',
-  'p-retry': 'https://www.npmjs.com/package/async-retry',
-  ora: 'https://www.npmjs.com/package/nanospinner',
-  'ts-results': 'https://www.npmjs.com/package/ts-results-es',
-  dayjs: 'https://www.npmjs.com/package/date-fns',
-  tldjs: 'https://www.npmjs.com/package/tldts',
-  'types-package-json': 'https://www.npmjs.com/package/@package-json/types',
-  'xxhash-wasm': 'https://www.npmjs.com/package/hash-wasm, significantly faster',
-  'string-width': 'https://www.npmjs.com/package/fast-string-width'
+  ms: npm('@lukeed/ms'),
+  classnames: npm('clsx'),
+  classcat: npm('clsx'),
+  'p-limit': `${npm('superlock')} / ${npm('@henrygd/queue')} / ${npm('async-sema')}`,
+  'p-retry': npm('async-retry'),
+  ora: npm('nanospinner'),
+  'ts-results': npm('ts-results-es'),
+  dayjs: npm('date-fns'),
+  tldjs: npm('tldts'),
+  'types-package-json': npm('@package-json/types'),
+  'xxhash-wasm': `${npm('hash-wasm')}, significantly faster`,
+  'string-width': npm('fast-string-width'),
+  lodash: 'https://es-toolkit.slash.page',
+  // escape string regexp
+  'escape-string-regexp': npm('foxts'),
+  'escape-regexp': npm('foxts'),
+  'lodash.escaperegexp': npm('foxts'),
+  'regex-escape': npm('foxts'),
+  // esacpe html
+  'escape-html': npm('foxts'),
+  'html-escaper': npm('foxts'),
+  'escape-goat': npm('foxts'),
+  'lodash.escape': npm('foxts')
 }).map(([key, value]) => ({ name: key, message: `Use ${value} instead.` }));
 
 const restricedImportBase = [
@@ -60,10 +74,10 @@ const restricedImportBase = [
 const restrictedImportWithImportNames = [
   { name: 'lodash', message: 'Avoid using type unsafe methods.', importNames: ['get'] },
   { name: 'lodash-es', message: 'Avoid using type unsafe methods.', importNames: ['get'] },
-  { name: 'lodash', message: 'Use https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser instead', importNames: ['cloneDeep'] },
-  { name: 'lodash-es', message: 'Use https://www.npmjs.com/package/rfdc for Node.js / https://www.npmjs.com/package/klona for Browser instead', importNames: ['cloneDeep'] },
-  { name: 'uuid', importNames: ['v4'], message: 'Use https://www.npmjs.com/package/@lukeed/uuid instead' },
-  { name: 'assert', importNames: ['deepEqual'], message: 'Use https://www.npmjs.com/package/dequal instead' },
+  { name: 'lodash', message: `Use ${npm('rfdc')} for Node.js / ${npm('klona')} for Browser instead`, importNames: ['cloneDeep'] },
+  { name: 'lodash-es', message: `Use ${npm('rfdc')} for Node.js / ${npm('klona')} for Browser instead`, importNames: ['cloneDeep'] },
+  { name: 'uuid', importNames: ['v4'], message: `Use ${npm('@lukeed/uuid')} instead` },
+  { name: 'assert', importNames: ['deepEqual'], message: `Use ${npm('dequal')} instead` },
   { name: 'react', importNames: ['useLayoutEffect'], message: 'Use https://foxact.skk.moe/use-isomorphic-layout-effect instead' }
 ];
 
