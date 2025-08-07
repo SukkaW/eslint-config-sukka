805dcc7f18f48d5e029117f597c8530c1b49a713
SukkaW <isukkaw@gmail.com>
2025-08-07 22:26:06 +0800

    refactor(markdown): simplify

--- packages/eslint-config-sukka/src/modules/markdown.ts
+++ packages/eslint-config-sukka/src/modules/markdown.ts
@@ -2,10 +2,5 @@ import type { FlatESLintConfigItem } from '@eslint-sukka/shared';
 import { default as eslint_markdown } from '@eslint/markdown';
 
 export function markdown(): FlatESLintConfigItem[] {
-  const eslint_markdown_config = eslint_markdown.configs.recommended;
-  return Array.isArray(eslint_markdown_config)
-    ? eslint_markdown_config
-    : [
-      eslint_markdown_config as any
-    ];
+  return eslint_markdown.configs.recommended;
 }
