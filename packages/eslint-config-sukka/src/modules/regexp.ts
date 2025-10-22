17d6fd367284c29971228c6e388d50d3dd4390ae
SukkaW <isukkaw@gmail.com>
2025-10-22 20:36:57 +0800

    refactor(regexp): simplify rules

--- packages/eslint-config-sukka/src/modules/regexp.ts
+++ packages/eslint-config-sukka/src/modules/regexp.ts
@@ -1,20 +1,15 @@
 import eslint_plugin_sukka from '@eslint-sukka/eslint-plugin-sukka-full';
 
-import { memo } from '@eslint-sukka/shared';
 import type { FlatESLintConfigItem } from '@eslint-sukka/shared';
-import * as eslint_plugin_regexp from 'eslint-plugin-regexp';
 import { configs as eslint_pluin_regexp_configs } from 'eslint-plugin-regexp';
 
 export function regexp(): FlatESLintConfigItem[] {
   return [
     eslint_plugin_sukka.configs.regexp,
+    eslint_pluin_regexp_configs['flat/recommended'],
     {
       name: 'sukka/regexp',
-      plugins: {
-        regexp: memo(eslint_plugin_regexp, 'eslint-plugin-regexp')
-      },
       rules: {
-        ...eslint_pluin_regexp_configs['flat/recommended'].rules,
         'regexp/strict': 'off' // we accepts Annex B regex from better-regex
       }
     }
