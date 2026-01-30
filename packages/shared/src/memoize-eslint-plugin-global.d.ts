10ffa6fb06fb8ccb0f34eea3d473a1298e64e563
SukkaW <isukkaw@gmail.com>
2026-01-30 11:28:52 +0800

    fix(shared): improve internal types

--- packages/shared/src/memoize-eslint-plugin-global.d.ts
+++ packages/shared/src/memoize-eslint-plugin-global.d.ts
@@ -1,4 +1,2 @@
-declare global {
-  // eslint-disable-next-line vars-on-top -- d.ts
-  var __ESLINT_PLUGIN_MEMO__: Record<string, unknown> | undefined;
-}
+// eslint-disable-next-line no-var -- d.ts
+declare var __ESLINT_PLUGIN_MEMO__: Record<string, unknown> | undefined;
